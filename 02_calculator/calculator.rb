641e8542a088da90532397756302469d939aa04a
Luminus Olumide Alabi <luminus@vedutanova.com>
2017-02-26 17:25:58 +0000

    Make Calculator tests pass, then refactor code using reduce method

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,45 @@
 #write your code here
+def add(one, two)
+  one + two
+end
+
+def subtract(one, two)
+  one - two
+end
+
+def sum array
+  # sum = 0
+  # if array.length > 0
+  #   array.each do |number|
+  #     sum += number
+  #   end
+  # end
+  # sum
+
+  array.reduce(:+) || 0
+
+end
+
+def multiply array
+  array.reduce(:*)
+end
+
+def power(one, two)
+  one ** two
+end
+
+def factorial number
+  # fact = 1
+  # if number > 0
+  #   (1..number).each do |num|
+  #     fact *= num
+  #   end
+  # else
+  #   fact = 0
+  # end
+
+  # fact
+
+  (1..number).reduce(:*) || 0
+
+end
