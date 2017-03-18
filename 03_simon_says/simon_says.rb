db13771c8170cee208408770805b6a0d55493ca0
Luminus Olumide Alabi <luminus@vedutanova.com>
2017-03-18 20:37:22 +0000

    Make Simon Says tests pass

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,29 @@
 #write your code here
+def echo greeting
+  greeting
+end
+
+def shout greeting
+  greeting.upcase
+end
+
+def repeat(greeting, number = 2)
+  number.times.collect{ greeting }.join(' ')
+end
+
+def start_of_word(word, number_of_chars)
+  word.chars[0..(number_of_chars - 1)].join
+end
+
+def first_word string
+  string.split[0]
+end
+
+def titleize string
+  ignored_words = ["and", "the", "over"]
+  if string.split.length > 1
+    string.split.each_with_index.map { |word, index| ignored_words.include?(word) && index > 0 ? word : word.capitalize! }.join(" ")
+  else
+    string.capitalize
+  end
+end
